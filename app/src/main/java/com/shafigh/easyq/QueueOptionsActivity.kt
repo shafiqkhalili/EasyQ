85d671dd98ba7cad52f9bc80cd4e08e60637df02
Shafigh Khalili <shafiqkhalili@gmail.com>
2020-05-01 21:57:40 +0200

    Fetch Place data from Places API

--- app/src/main/java/com/shafigh/easyq/QueueOptionsActivity.kt
+++ app/src/main/java/com/shafigh/easyq/QueueOptionsActivity.kt
@@ -1,38 +1,68 @@
 package com.shafigh.easyq
 
+import android.location.Address
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.TextView
+import androidx.annotation.RequiresApi
 import com.google.android.gms.common.api.ApiException
 import com.google.android.libraries.places.api.Places
 import com.google.android.libraries.places.api.model.Place
 import com.google.android.libraries.places.api.net.FetchPlaceRequest
+import kotlinx.android.synthetic.main.activity_queue_options.*
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.time.format.FormatStyle
 
 class QueueOptionsActivity : AppCompatActivity() {
 
+    lateinit var textViewHeader : TextView
+    lateinit var textViewAddress: TextView
+    lateinit var textViewDate : TextView
+
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_queue_options)
+
+        textViewHeader = findViewById(R.id.textViewBusiness)
+        textViewAddress = findViewById(R.id.textViewAddress)
+        textViewDate = findViewById(R.id.textViewDate)
+
+        val current = LocalDateTime.now()
+
+        val formatter = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM)
+        val formattedDate = current.format(formatter)
 
         // Define a Place ID.
         val placeId = "ChIJAysjm0xwX0YROPjAzVbwv6M"
 
         // Initialize Places.
-        Places.initialize(applicationContext, android.R.attr.apiKey.toString())
+        Places.initialize(applicationContext, "AIzaSyBhE-W27N5l__Pz4Ny1DrkXdC0irG5R37c")
         // Create a new Places client instance.
         val placesClient = Places.createClient(this)
         // Specify the fields to return.
         val placeFields: List<Place.Field> =
             listOf(
                 Place.Field.ID,
-                Place.Field.NAME
+                Place.Field.NAME,
+                Place.Field.ADDRESS,
+                Place.Field.OPENING_HOURS,
+                Place.Field.PHONE_NUMBER,
+                Place.Field.PRICE_LEVEL
             )
         // Construct a request object, passing the place ID and fields array.
         val request = FetchPlaceRequest.newInstance(placeId, placeFields)
 
         placesClient.fetchPlace(request).addOnSuccessListener { response ->
             val place: Place = response.place
-            Log.i("DEMO", "Place found: " + place.name)
+            Log.i("DEMO", "Place found: " + place.address)
+            textViewHeader.text = place.name
+            textViewAddress.text = place.address
+            textViewDate.text = formattedDate.toString()
+
         }.addOnFailureListener { exception ->
             if (exception is ApiException) {
                 val statusCode = exception.statusCode
